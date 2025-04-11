3368f37dda56dd7bf7b0f3936d343992daf3cfc8
WuKong0601 <22520168@gm.uit.edu.vn>
2025-04-11 21:24:54 +0700

    update images

--- portfolio-website/src/components/Hero.jsx
+++ portfolio-website/src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
-import {letters, professionTexts} from '../data/index'
+import {letters, professionTexts, aboutText} from '../data/index'
 
 const Hero = () => {
     const [hoveredLetter, setHoveredLetter] = useState(null)
@@ -17,10 +17,11 @@ const Hero = () => {
                 setIsRotating(false)
             }, 300)
         }, 5000)
+        return () => clearInterval(interval)
         
     },[])
   return (
-    <div className='w-full h-screen flex flex-col justify-center items-center'>
+    <div className='w-full h-screen flex flex-col justify-center items-center isolate'>
       <Navbar />
       <div className='flex flex-col md:items-center items-start xl:gap-y-10 gap-y-3 xl:mb-0 md:mb-20 ms-0'>
         <h1 className='flex flex-col xl:space-y-8 md:space-y-4 space-y-2 xl:text-6xl ms:text-4xl text-3xl md:font-normal font-bolder text-yellow-500'>
@@ -47,6 +48,13 @@ const Hero = () => {
             Developer
           </span>
         </h1>
+        <button className='xl:w-[400px] md:w-[300px] w-[270px] bg-gray-200 md:py-1 py-0 md:px-4 px-2 xl:text-2xl md:text-xl text-base text-gray-900 tracking-widest rounded-r-4xl flex justify-between items-center md:mr-auto md:mx-0 mx-auto'>
+          Read My Journey <i className='bx bx-book-open'></i>
+        </button>
+        <div className='lg:w-[600px] md:w-[500px] w-[350px] absolute left-1/2-translate-x-1/2 -z-10'>
+        <img src="images/road.png" alt="Road Image" className='w-full ms-auto' />
+        
+        </div>
       </div>
     </div>
   )
